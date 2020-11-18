691c6c95bd9b0d5d034eab03129e524e9bd37da5
Mohmed Anis <anishagwan@gmail.com>
2020-11-18 21:48:20 +0530

    Created schemas for lisign property schema and realtors schema

--- real_estate_btre/settings.py
+++ real_estate_btre/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'real_estate_btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'real-estate-db',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost'
     }
 }
 
